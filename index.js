61434726063b5fcbe5b0c8a8bf785c0de2b0d512
Joshua Ochsankehl <eternity336@gmail.com>
2023-02-16 21:01:58 -0600

    Mid Save for Capstone to SPA

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-function gradient_color(temp){
-    //takes a temp and determines color from a gradient
-    const low_temp = -100
-    const high_temp = 200
-    const r_low = 138
-    const g_low = 255
-    const b_low = 246
-    const r_high = 254
-    const g_high = 1
-    const b_high = 1
-    let perc = (temp + 100) /3
-    let perc_r = (r_low + r_high)/100*perc
-    let perc_g = (g_low + g_high)/100*perc
-    let perc_b = (b_low + b_high)/100*perc
-    return [perc_r, perc_g, perc_b]
+import { Header, Footer, Nav, Main } from './components';
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(state)}
+    ${Nav(store.Links)}
+    ${Main(state)}
+    ${Footer()}
+  `;
+  router.updatePageLinks();
+  console.log(state);
 }
 
-console.log(gradient_color(75))
-console.log(gradient_color(100))
+const router = new Navigo("/");
+router.on("/", () => render(store.Home)).resolve();
+router
+  .on({
+    "/": () => render(),
+    ":view": (params) => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    },
+  })
+  .resolve();
 
-function home(){
-    document.getElementById("home_frame").src = './clouds.html';
-}
-
-function about(){
-    document.getElementById("home_frame").src = './about.html';
-}
-
-function weather(){
-    document.getElementById("home_frame").src = './weathercoat.html';
-}
+  //add menu toggle to bars icon in nav bar
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+});
