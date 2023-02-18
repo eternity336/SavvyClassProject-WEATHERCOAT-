7ba7e726bfcceefab9de5ca07e6b3030ce95cd7a
Joshua Ochsankehl <eternity336@gmail.com>
2023-02-17 18:39:24 -0600

    Updated application to be an SPA (Works but need to finish pages)

---
+++ components/views/home.js
@@ -0,0 +1,9 @@
+import html from "html-literal";
+
+export default () => html`
+<section id="main">
+    <p>
+        HOME PAGE
+    </p>
+</section>
+`;
---
+++ store/Weathercoat.js
@@ -0,0 +1,4 @@
+export default {
+  header: "My Weathercoat Page",
+  view: "Weathercoat"
+};
